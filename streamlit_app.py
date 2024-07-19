9fd2dd3e5ab7cc724087f6c23de1eb5c19c48b85
RuanPetrusV <ruan.petrus.vermeulen@gmail.com>
2024-07-19 12:49:02 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,8 @@
 import pandas as pd
 import plotly.express as px
 import streamlit as st
-
+import currency_converter as cucon
+cc = cucon.CurrencyConverter
 
 # Display title and text
 st.title("Week 1 - Data and visualization")
